9e7a969850b8a369498362f773fbbc250990e363
Felix <felixrunye@gmail.com>
2023-01-18 23:48:33 +0300

    added room price data

--- src/models/room.js
+++ src/models/room.js
@@ -6,6 +6,12 @@ const roomSchema = new mongoose.Schema({
         required: [true, 'Please enter room name'],
         trim: true,
         maxLength: [100, 'Room name cannot exceed 100 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter room price'],
+        trim: true,
+        maxLength: [4, 'Room price cannot exceed 4 characters'],
     }
 });
 
