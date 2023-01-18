b124ecaf27a8c0f4e6dac8645899d61e99b2248f
Felix <felixrunye@gmail.com>
2023-01-18 23:52:53 +0300

    added room price data

--- src/models/room.js
+++ src/models/room.js
@@ -12,7 +12,29 @@ const roomSchema = new mongoose.Schema({
         required: [true, 'Please enter room price'],
         trim: true,
         maxLength: [4, 'Room price cannot exceed 4 characters'],
-    }
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter room description'],
+    },
+    address: {
+        type: String, 
+        required: [true, 'Please enter room address'],
+    },
+    guestCapacity: {
+        type: Number,
+        required: [true, 'Please enter room guest capacity'],
+    },
+    numOfBeds: {
+        type: Number,
+        required: [true, 'Please enter number of beds in room'],
+    },
+    internet: {
+        type: Boolean,
+        required: [true, 'Please enter if room has internet'],
+        default: false
+    },
 });
 
 export default mongoose.model.Room || mongoose.model('Room', roomSchema)
