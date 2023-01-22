d6379e261059c86f380faa8e5879e422e5c4292c
Felix <felixrunye@gmail.com>
2023-01-22 23:17:06 +0300

    creating room model update

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,7 +111,7 @@ export default function Home() {
             </h2>
             <p className={inter.className}>
               Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+              with&nbsp;Felix.
             </p>
           </a>
         </div>
