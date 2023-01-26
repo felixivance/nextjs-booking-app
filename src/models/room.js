b76b26c2dabb3caaa51c91f0832a0f87f148b9a9
Felix <felixrunye@gmail.com>
2023-01-26 23:16:49 +0300

    models update

--- src/models/room.js
+++ src/models/room.js
@@ -110,4 +110,4 @@ const roomSchema = new mongoose.Schema({
     }
 });
 
-export default mongoose.model.Room || mongoose.model('Room', roomSchema)
+export default mongoose.models.Room || mongoose.model('Room', roomSchema)
