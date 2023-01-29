f09a5d177712b5bb33f68bd0e2936321377b4cd4
Felix <felixrunye@gmail.com>
2023-01-29 22:58:53 +0300

    Fix: logging

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,8 +53,7 @@ export default function Home() {
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer">
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+            <h2 className={inter.className}> Docs <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
