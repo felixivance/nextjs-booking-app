c3cc0353b99f4055a215258cb60705c31ac31bd9
Felix <felixrunye@gmail.com>
2023-01-31 09:46:07 +0300

    added files

---
+++ src/utils/seeder.js
@@ -0,0 +1,31 @@
+const Room = require('../models/room');
+
+const mongoose = require('mongoose');
+
+const rooms =  require('../data/rooms.json');
+
+mongoose.connect('mongodb://0.0.0.0:27017/bookit', {
+    // useCreateIndex: true, //not supported
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    // useFindAndModify: false, //not supported
+    family: 4
+})
+.then(con => console.log("MongoDB Database Connected!"))
+.catch(err => console.log(err))
+
+
+const seedRooms = async () => {
+    try{
+        await Room.deleteMany();
+        console.log('Rooms are deleted');
+
+        await Room.insertMany(rooms);
+        console.log("all rooms are added");
+    }catch(error){
+        console.log(error.message);
+        process.exit();
+    }
+}
+
+seedRooms();
